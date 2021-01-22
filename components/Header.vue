424378e58354060c9d9b586ec9e4bf3e35f21b3a
Littletreasure <ruth_sargent@yahoo.co.uk>
2021-01-22 23:29:53 +0000

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <img class="header__image" src="../assets/cake hunt logo.jpg" />
+    <h4>Gallery</h4>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 60px;
+  background: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  &__image {
+    width: 60px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+  <Header />
+    <img src="../assets/cake hunt logo.jpg" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: $colour-pink;
+  margin: 0 auto;
+  min-height: 100vh;  
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  
+  img {
+    width: 40%;
+    border: 1px solid $colour-pink;
+  }
+}
+
+
+</style>
